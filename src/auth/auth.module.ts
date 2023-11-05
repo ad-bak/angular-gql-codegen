93ab1c625ab89c249fdd4dc8a32fed3635c2fe40
Adnan <adbak.dev@gmail.com>
2023-11-05 01:34:55 +0100

    fc

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { UsersModule } from '../users/users.module';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+
+import { LocalStrategy } from './strategies/local.strategy';
+import { JwtStrategy } from './strategies/jwt.strategy';
+
+@Module({
+  imports: [
+    UsersModule,
+    JwtModule.registerAsync({
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: `${configService.get('JWT_EXPIRATION')}s`,
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
+})
+export class AuthModule {}
---
+++ src/database/abstract.reposityory.ts
@@ -0,0 +1,31 @@
+import { FilterQuery, Model, Types } from 'mongoose';
+import { AbstractDocument } from './abstract.schema';
+import { Logger, NotFoundException } from '@nestjs/common';
+
+export abstract class AbstractRepository<TDocument extends AbstractDocument> {
+  protected abstract readonly logger: Logger;
+
+  constructor(protected readonly model: Model<TDocument>) {}
+
+  async create(document: Omit<TDocument, '_id'>): Promise<TDocument> {
+    const createDocument = new this.model({
+      ...document,
+      _id: new Types.ObjectId(),
+    });
+    return (await createDocument.save()).toJSON() as unknown as TDocument;
+  }
+
+  async findOne(filterQuery: FilterQuery<TDocument>): Promise<TDocument> {
+    const document = await this.model.findOne(filterQuery, {}, { lean: true });
+
+    if (!document) {
+      this.logger.warn(
+        `Document not found in ${this.model.modelName} collection`,
+        filterQuery,
+      );
+      throw NotFoundException;
+    }
+
+    return document as unknown as TDocument;
+  }
+}
